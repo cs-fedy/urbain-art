fe81d17aac96fa2564d40424333afc1f95716953
cs-fedy <fedi.abd01@gmail.com>
2023-11-05 20:35:48 +0100

    newsletter responsive done

--- src/components/features/newsletter.tsx
+++ src/components/features/newsletter.tsx
@@ -5,35 +5,38 @@ import Image from "next/image"
 
 export default function Newsletter() {
 	return (
-		<div className='relative flex w-full items-center bg-urbain-black'>
-			<div className='w-1/2'>
+		<div className='relative flex w-full flex-col-reverse items-center gap-y-16 bg-urbain-black lg:flex-row'>
+			<div className='w-full lg:w-1/2'>
 				<Image
 					src={newsletter}
 					alt='newsletter image'
+					className='object-contain'
 					width={960}
 					height={600}
 				/>
 			</div>
-			<div className='mx-20 flex flex-col items-start space-y-12'>
-				<div className='flex flex-col items-start space-y-2'>
-					<h2 className='max-w-2xl font-play-fair text-6xl leading-snug tracking-wider text-urbain-white'>
+
+			<div className='mx-20 flex flex-col items-center space-y-12 py-10 lg:items-start lg:py-0'>
+				<div className='flex flex-col items-center space-y-2 lg:items-start'>
+					<h2 className='w-full text-center font-play-fair text-3xl leading-snug tracking-wider text-urbain-white sm:text-4xl md:text-5xl lg:max-w-2xl lg:text-left lg:text-6xl'>
 						Rejoignez notre Newsletter
 					</h2>
-					<span className='max-w-lg text-urbain-white'>
+					<span className='w-full text-center text-sm text-urbain-white md:text-base lg:max-w-lg lg:text-left'>
 						Recevez nos nouveautés, nos actualités ainsi que nos évènements.
 					</span>
 				</div>
-				<div className='flex w-3/4 flex-col items-start space-y-8'>
+
+				<div className='flex w-full flex-col items-center space-y-8 lg:w-3/4 lg:items-start'>
 					<Input
 						type='email'
 						name='email'
 						placeholder='Enter your email'
-						className='rounded-lg text-white'
+						className='w-full rounded-lg text-white lg:w-max'
 					/>
 					<Box
 						component='button'
-						className='bg-urbain-white text-urbain-black hover:bg-slate-100'>
-						S’inscrire
+						className='w-full bg-urbain-white text-urbain-black hover:bg-slate-100 lg:w-max'>
+						<span className='w-full text-center'>S’inscrire</span>
 					</Box>
 				</div>
 			</div>
