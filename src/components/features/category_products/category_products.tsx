b1b8583dad2f647fde032cb807acff87f35185f4
cs-fedy <fedi.abd01@gmail.com>
2023-11-09 15:42:07 +0100

    fixed category page

--- src/components/features/category_products/category_products.tsx
+++ src/components/features/category_products/category_products.tsx
@@ -1,6 +1,8 @@
+import grayLogo from "@/../public/svg/gray_logo.svg"
 import Box from "@/components/common/box"
 import Slider from "@/components/common/slider"
 import { listProducts } from "@/lib/api"
+import Image from "next/image"
 import { SubCategory } from "../categories/types"
 import ProductItem from "../product/product_item"
 
@@ -14,7 +16,7 @@ export default async function CategoryProducts({
 	limit,
 }: CategoryProductsProps) {
 	const products = await listProducts({
-		// filter: { subCategory: subCategory.tag },
+		filter: { subCategory: subCategory.tag },
 		limit,
 	})
 
@@ -23,7 +25,11 @@ export default async function CategoryProducts({
 
 	return (
 		<div className='relative w-full overflow-hidden  pt-[4.75rem] lg:pt-0'>
-			<div className='mx-auto flex h-full w-4/5 flex-col items-center gap-y-12 py-16 lg:pt-32'>
+			<div className='absolute -bottom-28 -left-48 z-0 h-[54rem] w-[45rem]'>
+				<Image src={grayLogo} alt='urbain art gray logo' fill />
+			</div>
+
+			<div className='relative z-20 mx-auto flex h-full w-4/5 flex-col items-center gap-y-12 py-16 lg:pt-32'>
 				<div className='flex w-full flex-col items-start gap-y-6'>
 					<h1 className='w-full text-center font-play-fair text-3xl font-bold leading-snug tracking-wider text-urbain-black sm:text-4xl md:text-5xl lg:max-w-2xl lg:text-left lg:text-6xl'>
 						{subCategory.title}
